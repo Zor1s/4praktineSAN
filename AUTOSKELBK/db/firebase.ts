19a00ffb7675b85b1cba6337f5b8cce9a95d710b
Dainius <169063817+Zor1s@users.noreply.github.com>
2025-11-13 22:23:21 +0200

    Update firebase.ts

--- AUTOSKELBK/db/firebase.ts
+++ AUTOSKELBK/db/firebase.ts
@@ -6,14 +6,14 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_wRI1KDDA0x9xvQbi2MIgzqDaQX5sn1E",
-  authDomain: "autoskelbk.firebaseapp.com",
-  databaseURL: "https://autoskelbk-default-rtdb.firebaseio.com",
-  projectId: "autoskelbk",
-  storageBucket: "autoskelbk.appspot.com", 
-  messagingSenderId: "115076600985",
-  appId: "1:115076600985:web:040946f83374f27d41d408",
-  measurementId: "G-32BL2243H4",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "", 
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 
